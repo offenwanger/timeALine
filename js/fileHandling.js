5a45d9414743c2458ac2075e32f15590f3740642
Anna Offenwanger <dev@offenwanger.ca>
2022-04-08 11:21:09 +0200

    add file handling

---
+++ js/fileHandling.js
@@ -0,0 +1,12 @@
+let FileHandler = function() {
+    async function getDataFile() {
+        let fileHandle = await window.showOpenFilePicker();
+        let file = await fileHandle[0].getFile();
+        let contents = await file.text();
+        return Papa.parse(contents);
+    }
+
+    return {
+        getDataFile,
+    }
+}();
