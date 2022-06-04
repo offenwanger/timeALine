5857daf623cb7febdbd1068d184243ba1caaee71
Anna Offenwanger <dev@offenwanger.ca>
2022-06-04 11:56:00 -0400

    add constants

---
+++ js/constants.js
@@ -0,0 +1,10 @@
+const DataTypes = {
+    TEXT: 'text',
+    NUM: 'num',
+    TIME_BINDING: 'timebinding'
+}
+
+const TimeBindingTypes = {
+    PLACE_HOLDER: 'place_holder',
+    TIMESTRAMP: 'timestamp',
+}
