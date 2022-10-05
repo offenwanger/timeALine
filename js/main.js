cb147177937528ed051ce08a9ffe20882a2342a2
Anna Offenwanger <dev@offenwanger.ca>
2022-10-05 13:24:18 +0200

    remove mousemove from main

--- js/main.js
+++ js/main.js
@@ -533,6 +533,11 @@ document.addEventListener('DOMContentLoaded', function (e) {
         mDataPointController.onPointerMove(coords);
         mIronController.onPointerMove(coords);
         mStrokeController.onPointerMove(coords);
+
+        $('#mode-indicator-div').css({
+            left: e.pageX + 10,
+            top: e.pageY + 10
+        });
     });
 
     $(document).on("pointerup", function (e) {
@@ -1047,13 +1052,6 @@ document.addEventListener('DOMContentLoaded', function (e) {
         // start hidden
         this.hide();
     }
-
-    $(document).on('mousemove', function (e) {
-        $('#mode-indicator-div').css({
-            left: e.pageX + 10,
-            top: e.pageY + 10
-        });
-    });
 
     /** useful test and development function: */
     // $(document).on('pointerover pointerenter pointerdown pointermove pointerup pointercancel pointerout pointerleave gotpointercapture lostpointercapture abort afterprint animationend animationiteration animationstart beforeprint beforeunload blur canplay canplaythrough change click contextmenu copy cut dblclick drag dragend dragenter dragleave dragover dragstart drop durationchange ended error focus focusin focusout fullscreenchange fullscreenerror hashchange input invalid keydown keypress keyup load loadeddata loadedmetadata loadstart message mousedown mouseenter mouseleave mousemove mouseover mouseout mouseup mousewheel offline online open pagehide pageshow paste pause play playing popstate progress ratechange resize reset scroll search seeked seeking select show stalled storage submit suspend timeupdate toggle touchcancel touchend touchmove touchstart transitionend unload volumechange waiting wheel', function (e) {
