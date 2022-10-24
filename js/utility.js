bf223cf0e3bc24230c655224d827ccef47c2d358
Anna Offenwanger <dev@offenwanger.ca>
2022-10-24 21:40:56 +0200

    improve validation and error handling

--- js/utility.js
+++ js/utility.js
@@ -185,7 +185,7 @@ let MathUtil = function () {
     }
 
     function isNumeric(val) {
-        return typeof val == 'number';
+        return typeof val == 'number' && !isNaN(val);
     }
 
     return {
@@ -999,6 +999,11 @@ function CanvasMask(canvas, x, y, width, height) {
     let mContext = canvas.getContext("2d", { willReadFrequently: true });
 
     this.isCovered = function (coords) {
+        if (!coords || !DataUtil.isNumeric(coords.x) || !DataUtil.isNumeric(coords.y)) {
+            console.error("Invalid mask coords!", coords);
+            return false;
+        }
+
         if (coords.x < mX || coords.y < mY || coords.x > mX + mWidth || coords.y > mY + mHeight) return false;
         return mContext.getImageData(Math.round(coords.x - mX), Math.round(coords.y - mY), 1, 1).data[3] > 0;
     }
